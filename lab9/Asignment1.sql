c6350f980171ca5525d803f581d4328b33b7a5b6
Nguyen Trung HIeu <65000911+hieuntth2002015@users.noreply.github.com>
2020-08-13 19:47:44 +0700

    lab9

---
+++ lab9/Asignment1.sql
@@ -0,0 +1,158 @@
+﻿CREATE DATABASE Asignment01
+GO
+USE Asignment01
+GO
+
+CREATE TABLE NguoiMua (
+  Makh varchar(10) PRIMARY KEY,
+  Nguoidathang nvarchar(50),
+  diachi nvarchar (50),
+  dienthoai CHAR(9),
+  CONSTRAINT chk_dt CHECK (dienthoai not like '%[^0-9]%') 
+)
+INSERT INTO NguoiMua VALUES (123, N'Nguyễn Văn An', N'111 Nguyễn Trãi, Thanh Xuân, Hà Nội', '987654321'),
+                            (124, N'Nguyễn Văn Anh', N'123 Nguyễn Trãi, Thanh Xuân, Hà Nội', '123456789')
+GO
+CREATE TABLE SanPham (
+Masp varchar(10) PRIMARY KEY,
+Tensp nvarchar (30),
+Mota nvarchar (50),
+Giaban Money CHECK (Giaban>0)
+)
+INSERT INTO SanPham VALUES ('T1', N'Máy Tính T450',N'Máy Mới Nhập', 1000),
+                           ('T2', N'Điện Thoại Nokia5670', N'Điện Thoại đang hot', 200),
+						   ('T3', N'Máy In Samsung450',N'Máy in đang ế', 100)
+GO
+CREATE TABLE DonDatHang (
+ Madathang int PRIMARY KEY,
+ Makh varchar(10),
+ Ngaydathang DATETIME,
+ Soluong int CHECK (Soluong >0),
+ Tongtien money
+ CONSTRAINT fk_NguoiMua FOREIGN KEY (Makh) REFERENCES NguoiMua(Makh)
+ )
+ INSERT INTO DonDatHang VALUES (111, 123, '2011-11-11', 2, 1500)
+ INSERT INTO DonDatHang VALUES (222, 124, '2011-11-11', 1, 1300)
+
+GO
+CREATE TABLE Donhangchitiet (
+ Makh varchar(10),
+ Madonhang int PRIMARY KEY,
+ Masp varchar(10),
+ Giaban Money CHECK (Giaban >0),
+ Soluongmua int
+ CONSTRAINT fk_SanPham FOREIGN KEY (Masp) REFERENCES SanPham(Masp),
+ CONSTRAINT fk_Khanhhang FOREIGN KEY (Makh) REFERENCES NguoiMua(Makh)
+ )
+GO
+INSERT INTO Donhangchitiet VALUES (123,11,'T1', 1000, 1)
+INSERT INTO Donhangchitiet VALUES (123,12,'T2', 200, 2)
+INSERT INTO Donhangchitiet VALUES (123,13,'T3', 100, 1)
+
+SELECT * FROM NguoiMua
+SELECT * FROM SanPham
+SELECT * FROM DonDatHang
+SELECT * FROM Donhangchitiet
+
+--4. Viết các câu lênh truy vấn để
+--a) Liệt kê danh sách khách hàng đã mua hàng ở cửa hàng
+SELECT Makh, Nguoidathang
+FROM NguoiMua
+--b) Liệt kê danh sách sản phẩm của của hàng 
+SELECT Masp, Tensp, Mota, Giaban
+FROM SanPham
+--c) Liệt kê danh sách các đơn đặt hàng của cửa hàng.
+SELECT Madonhang,Masp, Giaban, Soluongmua
+FROM Donhangchitiet
+
+--5. Viết các câu lệnh truy vấn để
+--a) Liệt kê danh sách khách hàng theo thứ thự alphabet
+SELECT Makh, Nguoidathang 
+FROM NguoiMua
+ORDER BY Nguoidathang
+
+--b) Liệt kê danh sách sản phẩm của cửa hàng theo thứ thự giá giảm dần
+SELECT Masp, Tensp, Mota, Giaban
+FROM SanPham
+ORDER BY Giaban DESC
+--c) Liệt kê các sản phẩm mà khách hàng Nguyễn Văn An đã mua
+SELECT Nguoidathang,Tensp, Mota, Mota, Soluongmua, Donhangchitiet.Giaban
+FROM Donhangchitiet 
+   JOIN NguoiMua
+          ON NguoiMua.Makh = Donhangchitiet.Makh
+   JOIN SanPham 
+          ON SanPham.Masp = Donhangchitiet.Masp
+WHERE Nguoidathang = N'Nguyễn Văn An'
+--6. Viết các câu lệnh truy vấn để
+--a) Số khách hàng đã mua ở cửa hàng.
+SELECT Nguoidathang, COUNT(*) AS Sokhanhang
+FROM NguoiMua
+Group by Nguoidathang
+
+--b) Số mặt hàng mà cửa hàng bán
+SELECT Tensp, Madonhang, Soluongmua AS N'Số Lượng đã bán'
+FROM SanPham
+     JOIN Donhangchitiet
+	    ON Donhangchitiet.Masp = SanPham.Masp
+--c) Tổng tiền của từng đơn hàng.
+SELECT Nguoidathang, SUM(Giaban) as N'Tổng tiền'
+FROM Donhangchitiet, NguoiMua
+Group by Nguoidathang
+
+--7. Thay đổi những thông tin sau từ cơ sở dữ liệu
+--a) Viết câu lệnh để thay đổi trường giá tiền của từng mặt hàng là dương(>0).ALTER TABLE SanPhamADD CONSTRAINT SP CHECK ( Giaban >0)
+--b) Viết câu lệnh để thay đổi ngày đặt hàng của khách hàng phải nhỏ hơn ngày hiện tại.  
+ALTER TABLE DonDatHang
+ADD CONSTRAINT Thaydoi CHECK (Ngaydathang < GETDATE())
+--c) Viết câu lệnh để thêm trường ngày xuất hiện trên thị trường của sản phẩm.ALTER TABLE SanPham
+ADD Ngayxuathien DATETIME CHECK (Ngayxuathien < GETDATE())
+--8. Thực hiện các yêu cầu sau
+--a) Đặt chỉ mục (index) cho cột Tên hàng và Người đặt hàng để tăng tốc độ truy vấn dữ liệu trên các cột này
+CREATE INDEX SanPham_Nguoidathang
+on SanPham(Tensp, Giaban, Mota)
+
+CREATE INDEX Nguoi_dat_hang
+ON NguoiMua(Makh, Nguoidathang, diachi, dienthoai)
+
+--b) Xây dựng các view sau đây:
+--◦ View_KhachHang với các cột: Tên khách hàng, Địa chỉ, Điện thoại
+--◦ View_SanPham với các cột: Tên sản phẩm, Giá bán
+--◦ View_KhachHang_SanPham với các cột: Tên khách hàng, Số điện thoại, Tên sản
+--phẩm, Số lượng, Ngày muaCREATE VIEW KhachHang asSELECT Nguoidathang, diachi, dienthoaiFROM NguoiMuaCREATE VIEW View_SanPham asSELECT Tensp, GiabanFROM SanPham
+CREATE VIEW KhachHang_SanPham AS
+SELECT Makh, Ngaydathang
+FROM DonDatHang(SELECT Nguoidathang, dienthoai, tensp, SoluongmuaFROM Donhangchitiet 
+   JOIN NguoiMua
+          ON NguoiMua.Makh = Donhangchitiet.Makh
+   JOIN SanPham 
+          ON SanPham.Masp = Donhangchitiet.Masp
+)
+--c) Viết các Store Procedure (Thủ tục lưu trữ) sau:
+--◦ SP_TimKH_MaKH: Tìm khách hàng theo mã khách hàng
+
+CREATE PROCEDURE TimKH_MaKH AS
+BEGIN 
+  SELECT Nguoidathang 
+  FROM NguoiMua
+  ORDER BY Makh;
+END;
+      exec TimKH_MaKH
+--◦ SP_TimKH_MaHD: Tìm thông tin khách hàng theo mã hóa đơn
+CREATE PROCEDURE TimKH_MaHD AS
+BEGIN
+  SELECT Nguoidathang, diachi, dienthoai
+  FROM DonDatHang as ddt
+  JOIN NguoiMua as nm
+     ON ddt.Makh = nm.Makh
+	 ORDER BY Madathang
+END;
+EXEC TimKH_MaHD
+--◦ SP_SanPham_MaKH: Liệt kê các sản phẩm được mua bởi khách hàng có mã được
+--truyền vào Store.
+CREATE PROCEDURE SanPham_MakH AS
+BEGIN 
+  SELECT Tensp, Mota, Giaban
+  FROM SanPham as sp
+  JOIN Donhangchitiet as dhct
+     ON dhct.Masp = sp.Masp
+   ORDER BY Madathang
---
+++ lab9/Asignment2.sql
@@ -0,0 +1,76 @@
+﻿CREATE DATABASE Assignment_02
+GO
+USE Assignment_02
+GO
+
+CREATE TABLE Company(
+  CompanyCode int PRIMARY KEY,
+  CompanyName varchar(10),
+  Address varchar(10),
+  Phone CHAR(6),
+  CONSTRAINT chk_dt CHECK (Phone not like '%[^0-9]%'),
+)
+GO
+CREATE TABLE Product (
+  ProductCode int PRIMARY KEY,
+  ProductName varchar(50),
+  CompanyCode int,
+  Descriptions nvarchar(60),
+  Unti nvarchar(10),
+  Price money,
+  Amount int,
+  CONSTRAINT fk_sp
+  FOREIGN KEY (CompanyCode) 
+  REFERENCES Company(CompanyCode)
+)
+GO
+
+--3. Viết các câu lệnh để thêm dữ liệu vào các bảngINSERT INTO Company values (123, 'Asus', 'USA' ,'098323'),             (124, 'Nokia', 'Finland', '054345'),			 (125, 'Samsung','Korea','010312')INSERT INTO Product values (1, 'Laptop T450',123,'Old import machine','Piece', 1000, 10),                           (2, 'TelePhone Nokia 5670',124,'The Phone is hot','Piece', 200,200),						   (3, 'Printer Samsung 450', 125,'The priter is normal','Piece', 100, 10)INSERT INTO Product values(4, 'Computer GT', 126, 'The Computer is Expense', 'Price', 10000, 0)UPDATE Product SET AMOUNT = 0 WHERE ProductCode = 4--4. Viết các câu lênh truy vấn để--a) Hiển thị tất cả các hãng sản xuất.
+SELECT CompanyCode, CompanyName,  Address varchar, Phone
+ FROM Company 
+--b) Hiển thị tất cả các sản phẩm.
+
+SELECT ProductCode, ProductName, Descriptions, Unti, Price
+FROM Product
+
+--5.Viết các câu lệnh truy vấn để 
+--a) Liệt kê danh sách hãng theo thứ thự ngược với alphabet của tên
+SELECT * FROM Company
+ORDER BY CompanyName DESC
+--b) Liệt kê danh sách sản phẩm của cửa hàng theo thứ thự giá giảm dần.
+SELECT * FROM Product
+ORDER BY Price DESC
+--c) Hiển thị thông tin của hãng Asus
+SELECT * FROM Company 
+WHERE CompanyName = 'Asus'
+--d) Liệt kê danh sách sản phẩm còn ít hơn 11 chiếc trong kho
+SELECT * FROM Product 
+WHERE Amount < 11
+--e) Liệt kê danh sách sản phẩm của hãng AsusSELECT ProductCode, ProductName, Descriptions, Unti, Price, AmountFROM Product where Companycode IN (SELECT CompanyCode FROM Company WHERE CompanyName = 'Asus')--6. Viết các câu lệnh truy vấn để lấ--a) Số hãng sản phẩm mà cửa hàng cóSELECT CompanyName, Count(*) as SoHang_SanPhamFROM Company GROUP BY CompanyName--b) Số mặt hàng mà cửa hàng bánSELECT ProductName, Count(*) as Mathang_CuaHangbanFROM ProductGROUP BY ProductName--c) Tổng số loại sản phẩm của mỗi hãng có trong cửa hàng.SELECT CompanyName, Count(*) as Tong_SoLoai_Mahang_MoiHangFROM ProductJOIN CompanyON Company.CompanyCode = Product.CompanyCodeGROUP BY CompanyName  --d) Tổng số đầu sản phẩm của toàn cửa hàngSELECT Count(*) as Mathang_CuaHangbanFROM Product--7. Thay đổi những thay đổi sau trên cơ sở dữ liệu--a) Viết câu lệnh để thay đổi trường giá tiền của từng mặt hàng là dương(>0)ALTER TABLE Product  ADD CHECK (Price >0)--b) Viết câu lệnh để thay đổi số điện thoại phải bắt đầu bằng 0.ALTER TABLE CompanyADD CHECK (Phone LIKE '0%')--c) Viết các câu lệnh để xác định các khóa ngoại và khóa chính của các bảngSELECT fk_spFROM Company--8. Thực hiện các yêu cầu sau
+--a) Thiết lập chỉ mục (Index) cho các cột sau: Tên hàng và Mô tả hàng để tăng hiệu suất truy vấn
+--dữ liệu từ 2 cột này
+
+CREATE INDEX IX_Product
+ON Product(ProductName, Descriptions)
+EXEC sp_helpindex 'Product'
+
+--b) Viết các View sau:
+--◦ View_SanPham: với các cột Mã sản phẩm, Tên sản phẩm, Giá bán
+CREATE VIEW View_SanPham as
+SELECT ProductCode, ProductName, Price
+FROM Product
+--View_SanPham_Hang: với các cột Mã SP, Tên sản phẩm, Hãng sản xuấ
+CREATE VIEW View_SanPham_Hang AS
+SELECT ProductCode, ProductName, CompanyName
+FROM Product 
+JOIN Company
+ON Product.CompanyCode = Company.CompanyCode
+--c) Viết các Store Procedure sau:
+--◦ SP_SanPham_TenHang: Liệt kê các sản phẩm với tên hãng truyền vào store
+ CREATE PROCEDURE SP_SanPham_TenHang
+AS 
+SELECT * FROM View_SanPham_Hang
+
+
+EXEC SP_SanPham_TenHang
+--SP_SanPham_Gia: Liệt kê các sản phẩm có giá bán lớn hơn hoặc bằng giá bán truyền vàoCREATE PROCEDURE Sp_SanPham_GiaAS SELECT * FROM ProductWHERE Price > 1000-- SP_SanPham_HetHang: Liệt kê các sản phẩm đã hết hàng (số lượng = 0)CREATE PROCEDURE Sp_SanPham_HetHangAS SELECT * FROM ProductWHERE Amount = 0EXEC Sp_SanPham_HetHang--d) Viết Trigger sau:--TG_Xoa_Hang: Ngăn không cho xóa hãngCREATE TRIGGER TG_Xoa_HangON CompanyFOR DELETE AS   BEGIN     declare @CompanyName varchar(10);	SELECT @CompanyName = CompanyName FROM deleted;	ROLLBACK TRANSACTION;	ENDDELETE FROM CompanyWHERE CompanyCode = 123--TG_Xoa_SanPham: Chỉ cho phép xóa các sản phẩm đã hết hàng (số lượng = 0)CREATE TRIGGER TG_Xoa_SanPhamON Product
